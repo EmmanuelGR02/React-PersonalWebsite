16ab496361314dfa32fc4ff60a19858513a8cc85
Emmanuel <e.gutierrezrivera02@gmail.com>
2025-02-12 20:02:21 -0500

    fixed font sized, added animation to tags

--- src/Components/Contact/Contact.tsx
+++ src/Components/Contact/Contact.tsx
@@ -1,56 +1,58 @@
+import { useEffect, useState } from "react";
 import Footer from "../../Footer/Footer"
 import "./Contact.css"
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import "animate.css";
 
 
 function Contact() {
+ 
+
   return (
     <>
-      <section id="contact">
-  
-        <h1 className="section-header">Contact</h1>
-      
+      <section className="contact" id="contact">
+
         <div className="contact-wrapper">
       
-          <div className="card">
-            <h1 className="contactTitle">Get in Contact</h1>
+          <div className="card animate__animated animate__backInUp">
+            <h1 className="contactTitle animate__animated animate__backInRight">Contact Me!</h1>
             <div className="row">
               <div className="col">
                 <div className="form-group">
-                  <label>Name</label>
-                  <input type="text"></input>
+                  <label className="animate__animated animate__backInLeft">Name</label>
+                  <input className="animate__animated animate__backInRight" type="text" placeholder="Your name"></input>
                 </div>
               </div>
 
               <div className="col">
                 <div className="form-group">
-                  <label>Email</label>
-                  <input type="text"></input>
+                  <label className="animate__animated animate__backInLeft">Email</label>
+                  <input className="animate__animated animate__backInRight" type="text" placeholder="Your email"></input>
                 </div>
               </div>
 
               <div className="col">
                 <div className="form-group">
-                  <label>Phone</label>
-                  <input type="text"></input>
+                  <label className="animate__animated animate__backInLeft">Phone</label>
+                  <input className="animate__animated animate__backInRight" type="text" placeholder="Your phone number"></input>
                 </div>
               </div>
 
               <div className="col">
                 <div className="form-group messageInput">
-                  <label>Message</label>
-                  <textarea></textarea>
+                  <label className="animate__animated animate__backInLeft">Message</label>
+                  <textarea className="message-box animate__animated animate__backInRight" placeholder="Type your message here."></textarea>
                 </div>
               </div>
 
               <div className="col">
-                <input type="submit" value="Submit"></input>
+                <input className="animate__animated animate__backInLeft" type="submit" value="Submit"></input>
               </div>
             </div>
         </div>
           
           
-            <div className="direct-contact-container">
+            <div className="direct-contact-container animate__animated animate__backInDown">
 
               <ul className="contact-list">
                 <li className="list-item"><i className="fa fa-map-marker fa-2x"><span className="contact-text place">Dayton, OH</span></i></li>
