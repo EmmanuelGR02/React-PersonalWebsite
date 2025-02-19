fd481b9aec4605ddcacfab635c31dbb1c23b611b
Emmanuel <e.gutierrezrivera02@gmail.com>
2025-02-18 20:16:25 -0500

    changed welcome page

--- src/Components/Welcome/Welcome.tsx
+++ src/Components/Welcome/Welcome.tsx
@@ -5,33 +5,15 @@ export default function Welcome() {
   const navigate = useNavigate();
 
   return (
-    <div id="welcome-page">
-      <div id="stars"></div>
-      <div id="stars2"></div>
-      <div id="stars3"></div>
-
-      <div id="parallax">
-        <div className="layer" data-depth="0.6">
-          <div className="some-space">
-            <h1>Welcome! Thanks for visiting my website.</h1>
-          </div>
+    <>
+      <div className="welcome-container">
+        <div className="background-animation"></div>
+        <div className="content">
+          <h1 className="title">Welcome</h1>
+          <p>Thanks for visiting.</p>
+          <button onClick={() => navigate("/home")}>Enter</button>
         </div>
-
-        <div className="layer" data-depth="0.4">
-          <div id="particles-js"></div>
-        </div>
-
-        <div className="buttonDiv">
-          <div id="third" className="buttonBox">
-            <button className="welcomeButton" onClick={() => navigate("/home")}>Continue to website</button>
-            <div className="border"></div>
-            <div className="border"></div>
-            <div className="border"></div>
-            <div className="border"></div>
-          </div>
-        </div>
-
       </div>
-    </div>
+    </>
   );
 }
